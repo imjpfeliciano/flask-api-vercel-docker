76c0f852e8dc54e8cbc224d492b28c330a78c1d1
Jonathan Peraza Feliciano <jperazafeliciano@gmail.com>
2023-08-23 17:08:54 -0600

    feat: add swagger ui

--- api/index.py
+++ api/index.py
@@ -1,6 +1,20 @@
 from flask import Flask, jsonify
+from flask_swagger_ui import get_swaggerui_blueprint
 
 app = Flask(__name__)
+
+SWAGGER_URL = '/api/docs'
+API_URL = '/static/swagger.json'
+
+swagger_ui_blueprint = get_swaggerui_blueprint(
+    SWAGGER_URL,
+    API_URL,
+    config={
+        'app_name': 'Flask Swagger UI'
+    }
+)
+
+app.register_blueprint(swagger_ui_blueprint, url_prefix=SWAGGER_URL)
 
 @app.route('/')
 def home():
