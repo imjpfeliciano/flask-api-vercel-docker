40d6139b675cbdfcca859330c8b564a5a7cab513
Jonathan Peraza Feliciano <jperazafeliciano@gmail.com>
2023-08-23 17:31:51 -0600

    feat: add vscode settings

--- api/index.py
+++ api/index.py
@@ -3,23 +3,21 @@ from flask_swagger_ui import get_swaggerui_blueprint
 
 app = Flask(__name__)
 
-SWAGGER_URL = '/api/docs'
-API_URL = '/static/swagger.json'
+SWAGGER_URL = "/api/docs"
+API_URL = "/static/swagger.json"
 
 swagger_ui_blueprint = get_swaggerui_blueprint(
-    SWAGGER_URL,
-    API_URL,
-    config={
-        'app_name': 'Flask Swagger UI'
-    }
+    SWAGGER_URL, API_URL, config={"app_name": "Flask Swagger UI"}
 )
 
 app.register_blueprint(swagger_ui_blueprint, url_prefix=SWAGGER_URL)
 
-@app.route('/')
-def home():
-    return jsonify({'message': 'Hello, World!'})
 
-@app.route('/api', strict_slashes=False)
+@app.route("/")
+def home():
+    return jsonify({"message": "Hello, World!"})
+
+
+@app.route("/api", strict_slashes=False)
 def api():
-    return jsonify({'message': 'Hello, API!'})
+    return jsonify({"message": "Hello, API!"})
